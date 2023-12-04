16b85780766e6f11aa4d5fad4763fe7903038c7e
Agostina Ravazenghi <67771188+Agosdev@users.noreply.github.com>
2023-12-04 00:24:33 -0300

    write  test file for MPBrick component

---
+++ users-client/src/tests/MPBrick.test.tsx
@@ -0,0 +1,29 @@
+import MPBrick from "../components/MPBrick";
+import { fireEvent, render, screen } from "@testing-library/react";
+import { test, describe, expect } from "vitest";
+
+describe("MPBrick", () => { 
+    
+    beforeEach(() => {
+        render(<MPBrick />)
+    })
+    
+    test('should show the course info and pay button', () => {
+        const button = screen.getByText('Buy FullStack Course');
+        const title = screen.getByText('FullStack Course');
+        const image = screen.getByAltText('course-image');
+
+        expect(title).toBeDefined();
+        expect(button).toBeDefined();
+        expect(image).toBeDefined();
+    });
+
+    test('should show the MP button when pay button is clicked', async () => {
+        const button = screen.getByText('Buy FullStack Course');
+        fireEvent.click(button);
+
+        expect(screen.findAllByText('Pay with Mercado Pago')).toBeDefined();
+
+        expect(screen.debug())
+    });
+})
